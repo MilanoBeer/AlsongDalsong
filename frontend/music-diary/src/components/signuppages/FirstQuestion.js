f822839b6e9c24f3bf66de484392ce26c741df87
MilanoBeer <tgd5753@gmail.com>
2023-05-15 14:50:13 +0900

    refactor: delete unused file

--- frontend/music-diary/src/components/signuppages/FirstQuestion.js
+++ frontend/music-diary/src/components/signuppages/FirstQuestion.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import "../../css/signuppages/SignupPageBook.css";
-import "../../css/signuppages/QuestionBox.css";
-import Button from "../Common/Button";
+import '../../css/signuppages/SignupPageBook.css';
+import '../../css/signuppages/QuestionBox.css';
+import Button from '../Common/Button';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
 // redux 확인중
-import { useSelector } from "react-redux";
-import { setNormalChoiceValue } from "../../store/store";
-import { useDispatch } from "react-redux";
+import { useSelector } from 'react-redux';
+import { setNormalChoiceValue } from '../../store/store';
+import { useDispatch } from 'react-redux';
 
 const SignupInfoBookcontainer = styled.div`
   width: 60%;
@@ -38,47 +38,47 @@ function FirstQuestion() {
   const dispatch = useDispatch();
 
   const onMoveQuestionTwo = () => {
-    navigate("/signup/question/two");
+    navigate('/signup/question/two');
   };
 
   // 슬픈 / 기쁜 / 에너지틱 / 평온한 노래 선택 -> dispatch
   const onClickSad = () => {
     setSad(!sad);
     if (!sad) {
-      setFirstAnswer("슬픈");
+      setFirstAnswer('슬픈');
       dispatch(setNormalChoiceValue(1));
     } else {
-      setFirstAnswer("   ");
+      setFirstAnswer('   ');
     }
   };
 
   const onClickHappy = () => {
     setHappy(!happy);
     if (!happy) {
-      setFirstAnswer("기쁜");
+      setFirstAnswer('기쁜');
       dispatch(setNormalChoiceValue(2));
     } else {
-      setFirstAnswer("   ");
+      setFirstAnswer('   ');
     }
   };
 
   const onClickEnergy = () => {
     setEnergytic(!energytic);
     if (!energytic) {
-      setFirstAnswer("에너지틱한");
+      setFirstAnswer('에너지틱한');
       dispatch(setNormalChoiceValue(3));
     } else {
-      setFirstAnswer("   ");
+      setFirstAnswer('   ');
     }
   };
 
   const onClickNormal = () => {
     setNormal(!normal);
     if (!normal) {
-      setFirstAnswer("평온한");
+      setFirstAnswer('평온한');
       dispatch(setNormalChoiceValue(4));
     } else {
-      setFirstAnswer("   ");
+      setFirstAnswer('   ');
     }
   };
 
@@ -93,47 +93,29 @@ function FirstQuestion() {
                 <div className="intro">
                   <h1>당신의 음악취향은? {storeEmail} </h1>
                   <h2>
-                    1. 나는 평소에 "<div className="answer">{firstAnswer}</div>"
-                    노래를 듣는다{" "}
+                    1. 나는 평소에 "<div className="answer">{firstAnswer}</div>" 노래를 듣는다{' '}
                   </h2>
 
                   <div className="first-row">
-                    <div
-                      className={sad ? "selected-box" : "question-box"}
-                      onClick={onClickSad}
-                    >
+                    <div className={sad ? 'selected-box' : 'question-box'} onClick={onClickSad}>
                       슬픈
                     </div>
-                    <div
-                      className={happy ? "selected-box" : "question-box"}
-                      onClick={onClickHappy}
-                    >
+                    <div className={happy ? 'selected-box' : 'question-box'} onClick={onClickHappy}>
                       기쁜
                     </div>
                   </div>
 
                   <div className="second-row">
-                    <div
-                      className={energytic ? "selected-box" : "question-box"}
-                      onClick={onClickEnergy}
-                    >
+                    <div className={energytic ? 'selected-box' : 'question-box'} onClick={onClickEnergy}>
                       에너지틱한
                     </div>
-                    <div
-                      className={normal ? "selected-box" : "question-box"}
-                      onClick={onClickNormal}
-                    >
+                    <div className={normal ? 'selected-box' : 'question-box'} onClick={onClickNormal}>
                       평온한
                     </div>
                   </div>
-                  
+
                   <div className="next-btn">
-                    <Button
-                      name="다음->"
-                      color="#AC5050"
-                      size="lg"
-                      onClick={onMoveQuestionTwo}
-                    />
+                    <Button name="다음->" color="#AC5050" size="lg" onClick={onMoveQuestionTwo} />
                   </div>
                 </div>
               </div>
